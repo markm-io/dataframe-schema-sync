a6df1fd77f86fe84cad5200c091d2996af6cd224
semantic-release <semantic-release>
2025-05-26 03:41:28 +0000

    1.0.0
    Automatically generated by python-semantic-release

--- docs/conf.py
+++ docs/conf.py
@@ -15,7 +15,7 @@ from sphinx.ext import apidoc
 project = "Dataframe Schema Sync"
 copyright = "2025, Mark Moreno"
 author = "Mark Moreno"
-release = "0.21.6"
+release = "1.0.0"
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
